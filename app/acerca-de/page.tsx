5ad9215a6382a582cc940ab8b3432a47016e8c8b
Jose Aguilar <josemanu0885@gmail.com>
2025-09-30 13:19:52 -0600

    revision

--- app/acerca-de/page.tsx
+++ app/acerca-de/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ArrowRight, Users, Rocket, Lightbulb, Code2 } from "lucide-react";
+import { ArrowRight, Users, Rocket, Lightbulb } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
