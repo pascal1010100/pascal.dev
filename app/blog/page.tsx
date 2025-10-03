341db1b0d32bde94fbfc1efa3484987c313b5670
Jose Aguilar <josemanu0885@gmail.com>
2025-10-03 04:17:17 -0600

    commit

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,8 +2,9 @@ import { notFound } from "next/navigation";
 import { blogPosts } from "@/data/blog";
 import Image from "next/image";
 
-export default async function BlogPostPage({ params }: { params: { slug: string } }) {
-  const post = blogPosts.find((p) => p.slug === params.slug);
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const post = blogPosts.find((p) => p.slug === slug);
 
   if (!post) return notFound();
 
